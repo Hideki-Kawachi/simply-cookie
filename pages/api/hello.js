a8eeb3e8155823c62745aea11233eecc5ef2478f
RonHidekiK <hideki.kawachi17@gmail.com>
2022-07-10 13:27:07 +0800

    fixed path

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "../../util/mongodb";
+import { connectToDatabase } from "../../db";
 
 export default async (req, res) => {
   const { db } = await connectToDatabase();
