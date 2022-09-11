4c5f43cf1f51e48a1894af1c637b5286b4b5a66f
RonHidekiK <hideki.kawachi17@gmail.com>
2022-09-11 20:48:31 +0800

    moved font import

--- pages/_app.js
+++ pages/_app.js
@@ -53,12 +53,6 @@ function __app({ Component, pageProps }) {
 				<title>Simply Cookie</title>
 				<meta name="description" content="Simply Cookie Website" />
 				<link rel="icon" href="/favicon.ico" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Caramel&family=Montserrat:wght@100;300;400;500&display=swap"
-					rel="stylesheet"
-				/>
 			</header>
 
 			<Component {...pageProps} />
